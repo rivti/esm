291faece59d3e50e2b8a2945f5aee84d6248e812
ddslima <47262096+ddslima@users.noreply.github.com>
2023-03-30 01:26:22 -0300

    Update control2.js

--- control2.js
+++ control2.js
@@ -1,10 +1,8 @@
 import React, { useEffect } from "https://esm.sh/react@18.2.0"
-import confetti from "https://esm.sh/canvas-confetti@1.6.0"
-
 export default MyControl () {
   return (
     <div>
-      <h1>Hello React! 222⚛️</h1>
+      <h1>Hello React!</h1>
       <p>Building user interfaces.</p>
     </div>
   )
